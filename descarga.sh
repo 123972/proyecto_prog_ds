5c17bf77faa4571a674c74ac5b2cfc2b6fed6ec4
Javier Valencia Goujon <valencig@hotmail.com>
2019-12-10 16:07:21 -0600

    Agregue ejecutable para descargar base de datos

---
+++ descarga.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O /data/Artists.csv "https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artists.csv"
+wget -O /data/Artworks.csv "https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.csv"
